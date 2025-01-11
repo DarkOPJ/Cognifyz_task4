80602490589cbde9753c3fd980da955abb384074
Boadu Aaron Jeffrey Bosomepem <jeffreyaaron84@gmail.com>
2025-01-11 15:44:17 +0000

    add password checklist to registration form and update styles for better UX

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import PasswordChecklist from "react-password-checklist";
+
 import { Link } from "react-router-dom";
 
 const RegisterModal = ({ isActive = false, close }) => {
@@ -6,15 +8,34 @@ const RegisterModal = ({ isActive = false, close }) => {
     ? "outer-8 popup-container active"
     : "outer-8 popup-container";
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const createNewUser = () => {
+    e.preventDefault();
+    const newUser = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    // if (password!== confirmPassword) {
+    //   alert("Passwords do not match");
+    //   return;
+    // }
+  };
+
   return (
     <div className={showModal}>
-      <form action="" className="booking-form">
+      <form onSubmit={createNewUser} action="" className="booking-form modal-booking-form">
         <button type="button" onClick={close} className="close-btn">
           <i className="fa-regular fa-circle-xmark"></i>
         </button>
 
         <div className="modal-form">
-
           <p className="signup-welcome open-sans">Hello!</p>
           <p className="signup-text open-sans">Sign Up to Get Started</p>
           <div className="booking-form1">
@@ -22,26 +43,52 @@ const RegisterModal = ({ isActive = false, close }) => {
               className="open-sans booking-name-email"
               placeholder="&#xf007; Full Name"
               type="text"
-              name="name"
-              id="name"
+              name="registerModalname"
+              id="registerModalname"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              autoComplete="name"
             />
             <input
               className="open-sans booking-name-email"
               placeholder="&#xf003; Email Address"
               type="email"
-              name="email"
-              id="email"
+              name="registerModalemail"
+              id="registerModalemail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="email"
             />
-          </div>
-
-          <div className="booking-form1">
             <input
               className="open-sans booking-name-email"
               placeholder="&#xf023; Password"
               type="password"
-              name="password"
-              id="password"
+              name="registerModalpassword"
+              id="registerModalpassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="new-password"
             />
+            <input
+              className="open-sans booking-name-email"
+              placeholder="&#xf023; Confirm Password"
+              type="password"
+              name="registerModalconfirmPassword"
+              id="registerModalconfirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              autoComplete="new-password"
+            />
+
+            <div className="">
+              <PasswordChecklist
+                rules={["minLength", "specialChar", "number", "capital", "match"]}
+                minLength={8}
+                value={password}
+                valueAgain={confirmPassword}
+              />
+            </div>
+
             <input
               className="signup-form-btn open-sans booking-name-email"
               type="submit"
@@ -49,14 +96,13 @@ const RegisterModal = ({ isActive = false, close }) => {
             />
           </div>
 
-        <p className="navigate-form open-sans">
-          I want to{" "}
-          <Link className="navigate-forms-link" to="/login">
-            Login
-          </Link>
-        </p>
+          <p className="navigate-form open-sans">
+            I want to{" "}
+            <Link className="navigate-forms-link" to="/login">
+              Login
+            </Link>
+          </p>
         </div>
-
       </form>
     </div>
   );
--- src/main/auth/Register.jsx
+++ src/main/auth/Register.jsx
@@ -1,13 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import PasswordChecklist from "react-password-checklist";
+
 import RegisterModal from "../../components/RegisterModal";
 
 const Register = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const close = () => {
     setModalOpen((prevState) => !prevState);
-  }
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const createNewUser = () => {
+    e.preventDefault();
+    const newUser = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    // if (password!== confirmPassword) {
+    //   alert("Passwords do not match");
+    //   return;
+    // }
+  };
   return (
     <div className="auth-outer-1">
       <Helmet>
@@ -24,7 +46,10 @@ const Register = () => {
             <Link className="auth-main-btn open-sans" to="/">
               Read More
             </Link>
-            <button onClick={() => setModalOpen((prevState) => !prevState)} className="auth-main-btn open-sans show-popup">
+            <button
+              onClick={() => setModalOpen((prevState) => !prevState)}
+              className="auth-main-btn open-sans show-popup"
+            >
               Sign Up
             </button>
           </div>
@@ -32,31 +57,59 @@ const Register = () => {
       </div>
 
       <div className="right-inner">
-        <form action="" className="signup-form">
+        <form onSubmit={createNewUser} className="signup-form">
           <p className="signup-welcome open-sans">Hello!</p>
           <p className="signup-text open-sans">Sign Up to Get Started</p>
 
           <input
-            className="open-sans"
+            className="open-sans booking-name-email"
             placeholder="&#xf007; Full Name"
             type="text"
             name="name"
             id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoComplete="name"
           />
           <input
-            className="open-sans"
+            className="open-sans booking-name-email"
             placeholder="&#xf003; Email Address"
             type="email"
             name="email"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
           />
           <input
-            className="open-sans"
+            className="open-sans booking-name-email"
             placeholder="&#xf023; Password"
             type="password"
             name="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="new-password"
           />
+          <input
+            className="open-sans booking-name-email"
+            placeholder="&#xf023; Confirm Password"
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            autoComplete="new-password"
+          />
+
+          <div className="password-check">
+            <PasswordChecklist
+              rules={["minLength", "specialChar", "number", "capital", "match"]}
+              minLength={8}
+              value={password}
+              valueAgain={confirmPassword}
+            />
+          </div>
           <input
             className="signup-form-btn open-sans"
             type="submit"
@@ -64,11 +117,14 @@ const Register = () => {
           />
 
           <p className="navigate-form open-sans">
-            I want to <Link className="navigate-forms-link" to="/login">Login</Link>
+            I want to{" "}
+            <Link className="navigate-forms-link" to="/login">
+              Login
+            </Link>
           </p>
         </form>
       </div>
-      <RegisterModal isActive={modalOpen} close={close}/>
+      <RegisterModal isActive={modalOpen} close={close} />
     </div>
   );
 };
